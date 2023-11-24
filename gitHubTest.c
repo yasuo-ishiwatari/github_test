075b72a34288f6f6f7cc1a727498695efbae11b9
Personala <y_ishiwatari@hotmail.co.jp>
2023-11-24 23:39:28 +0900

    4th commit by branch2

--- gitHubTest.c
+++ gitHubTest.c
@@ -6,6 +6,7 @@ int a = 10;
 int main(void)
 {
     printf("first commit test\n");
+    printf("4th commit by branch2\n");
     printf("second commit test\n");
     // third commit
     printf("third commit : a = %d\n",a);
