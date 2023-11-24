56f465b8728fc218788d74d68047b68162c49c08
Personala <y_ishiwatari@hotmail.co.jp>
2023-11-24 23:27:06 +0900

    third commit

--- gitHubTest.c
+++ gitHubTest.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+// third commit
+int a = 10;
+
 int main(void)
 {
     printf("first commit test\n");
     printf("second commit test\n");
+    // third commit
+    printf("third commit : a = %d\n",a);
 
     return 0;
 }
