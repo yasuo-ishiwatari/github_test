771d40c5494dc18cffd9a789e01ae64651e3b7df
Personala <y_ishiwatari@hotmail.co.jp>
2023-11-24 23:50:30 +0900

    5th commit by branch3

--- gitHubTest.c
+++ gitHubTest.c
@@ -3,10 +3,15 @@
 // third commit
 int a = 10;
 
+// 5th commit
+int b = 20;
+
 int main(void)
 {
     printf("first commit test\n");
     printf("4th commit by branch2\n");
+    // 5th commit
+    printf("5th commit by branch3 : b = %d\n",b);
     printf("second commit test\n");
     // third commit
     printf("third commit : a = %d\n",a);
